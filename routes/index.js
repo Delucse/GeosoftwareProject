55ec6ae1eefcfe930330e6421476d354c262423e
Phil <p_huef01@uni-muenster.de>
2019-08-13 12:46:47 +0200

    authorizationCheck + die Startseite ist nur als eingeloggter Nutzer erreichbar

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const authorizationCheck = require('../middleware/authorizationCheck');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
